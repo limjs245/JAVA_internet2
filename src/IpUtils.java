be01514a9ec9bc70da90462d22e7fe2784087853
omegaraoke <limjaesung732@gmail.com>
2025-11-18 13:36:11 +0900

    refactor: Improve IP address retrieval function again...

--- src/IpUtils.java
+++ src/IpUtils.java
@@ -8,21 +8,14 @@ public class IpUtils {
     public static String getLocalIp() {
         try {
             Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-
-            System.out.println("=== 네트워크 인터페이스 확인 ===");
             while (networkInterfaces.hasMoreElements()) {
                 NetworkInterface networkInterface = networkInterfaces.nextElement();
 
-                // 디버깅: 모든 인터페이스 정보 출력
-                System.out.println("\n인터페이스: " + networkInterface.getName());
-                System.out.println("  Display Name: " + networkInterface.getDisplayName());
-                System.out.println("  isLoopback: " + networkInterface.isLoopback());
-                System.out.println("  isVirtual: " + networkInterface.isVirtual());
-                System.out.println("  isUp: " + networkInterface.isUp());
-
                 // 1. 루프백(127.0.0.1), 2. 가상, 3. 비활성화 인터페이스 제외
-                if (networkInterface.isLoopback() || networkInterface.isVirtual() || !networkInterface.isUp()) {
-                    System.out.println("  -> 제외됨");
+                String name = networkInterface.getName().toLowerCase();
+                if (networkInterface.isLoopback() || !networkInterface.isUp() ||
+                        (!name.startsWith("eth") && !name.startsWith("en") &&
+                                !name.startsWith("wlan") && !name.startsWith("wl"))) {
                     continue;
                 }
 
@@ -30,25 +23,19 @@ public class IpUtils {
                 while (inetAddresses.hasMoreElements()) {
                     InetAddress inetAddress = inetAddresses.nextElement();
 
-                    System.out.println("  IP: " + inetAddress.getHostAddress());
-                    System.out.println("    instanceof Inet4Address: " + (inetAddress instanceof Inet4Address));
-                    System.out.println("    isSiteLocalAddress: " + inetAddress.isSiteLocalAddress());
-
                     // 4. IPv4 주소이면서, 5. 사설 IP 대역인 경우
                     if (inetAddress instanceof Inet4Address && inetAddress.isSiteLocalAddress()) {
-                        System.out.println("  -> 선택됨!");
                         return inetAddress.getHostAddress();
                     }
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
             return ">>> error: " + e.getMessage();
         }
         return ">> No Site Local IPv4 Address Found";
     }
 
     public static void main(String[] args) {
-        System.out.println("\n결과: " + getLocalIp());
+        System.out.println("My Local IP is: " + getLocalIp());
     }
 }
